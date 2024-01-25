05f8f71203b8397d263044ab04f6ae0c76be9d68
Mikhail Ramanovich <mikhail.ramanovich@standardbank.co.za>
2024-01-25 20:43:03 +0300

    java bug fix

--- Parser.java
+++ Parser.java
@@ -1,46 +1,51 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
+
 /**
  * This class is thread safe.
  */
 public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+    public String getContent(File file) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            StringBuilder output = new StringBuilder();
+            int bite;
+            while ((bite = fileInputStream.read()) != -1) {
+                output.append((char) bite);
+            }
+            return output.toString();
+        }
     }
-    return output;
-  }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
-      }
+
+    public String getContent(File file, Charset charset) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            return new String(fileInputStream.readAllBytes(), charset);
+        }
     }
-    return output;
-  }
-  public void saveContent(String content) {
-    FileOutputStream o = new FileOutputStream(file);
-    try {
-      for (int i = 0; i < content.length(); i += 1) {
-        o.write(content.charAt(i));
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+
+    public String getContentWithoutUnicode(File file) throws IOException {
+        try (FileInputStream i = new FileInputStream(file)) {
+            StringBuilder output = new StringBuilder();
+            int bite;
+            while ((bite = i.read()) != -1) {
+                if (bite < 0x80) {
+                    output.append((char) bite);
+                }
+            }
+            return output.toString().trim();
+        }
     }
-  }
+
+    public void saveContent(String content, File file) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            fileOutputStream.write(content.getBytes());
+        }
+    }
+
+    public void saveContent(String content, File file, Charset charset) throws IOException {
+        try (OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(file), charset)) {
+            outputStreamWriter.write(content);
+        }
+    }
+
 }
